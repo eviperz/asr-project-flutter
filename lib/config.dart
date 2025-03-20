585ca36dac18057269e1b6d4c0ea7b2dcc95e9c5
tirawat1 <tirawat.pst@gmail.com>
2025-03-20 14:10:22 +0700

    setUserId and setToken

--- lib/config.dart
+++ lib/config.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 // import 'package:shared_preferences/shared_preferences.dart';
+final FlutterSecureStorage _storage = FlutterSecureStorage();
 
 class AppConfig {
-  // static const String baseUrl = "http://192.168.1.37:8080";
-  static const String baseUrl = "http://localhost:8080";
+  static const String baseUrl = "http://192.168.1.37:8080";
+  // static const String baseUrl = "http://localhost:8080";
 
   static const String username = "admin";
   static const String password = "password";
@@ -15,20 +16,27 @@ class AppConfig {
       'Basic ${base64Encode(utf8.encode('$username:$password'))}';
 
   static String? userId;
+  static String? token;
 
-  static Future<void> loadUserId() async {
-    final prefs = await SharedPreferences.getInstance();
-    userId = prefs.getString("userId");
-  }
-
+  // Set user ID securely
   static Future<void> setUserId(String newUserId) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString("userId", newUserId);
+    await _storage.write(key: "userId", value: newUserId);
     userId = newUserId;
   }
 
+  // Get user ID securely
   static Future<String?> getUserId() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString("userId");
+    return await _storage.read(key: "userId");
+  }
+
+  // Set token securely
+  static Future<void> setToken(String newToken) async {
+    await _storage.write(key: "token", value: newToken);
+    token = newToken;
+  }
+
+  // Get token securely
+  static Future<String?> getToken() async {
+    return await _storage.read(key: "token");
   }
 }
